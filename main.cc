51c964fa4aaeef82ebf9e08ca5ad60bf6975cc7c
dkogan <dkogan@ad02fb43-ca80-4f7a-a0d8-6dd9ab058ac3>
2010-02-12 08:54:23 +0000

    now passing in the correct thread context

--- main.cc
+++ main.cc
@@ -60,7 +60,7 @@ int main(void)
         return 0;
 
     pthread_t cameraThread_id;
-    if(pthread_create(&cameraThread_id, NULL, &cameraThread, NULL) != 0)
+    if(pthread_create(&cameraThread_id, NULL, &cameraThread, &cam) != 0)
     {
         cerr << "couldn't start thread" << endl;
         return 0;
