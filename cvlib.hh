6a9fccbb9151c68448543056f10b71a481394493
Dima Kogan <dima@secretsauce.net>
2011-06-08 23:32:31 -0700

    added an include needed by opencv

--- cvlib.hh
+++ cvlib.hh
@@ -1,6 +1,11 @@
 #ifndef __CV_LIB_HH__
 #define __CV_LIB_HH__
 
+// opencv header require this
+#ifdef __cplusplus
+#include <cstddef>
+#endif
+
 #ifndef OPENCV_DEBIAN_PACKAGES
   #include <opencv2/core/core_c.h>
   #include <opencv2/imgproc/imgproc_c.h>
